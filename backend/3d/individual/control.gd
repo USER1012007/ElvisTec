c92978ea540c15bd27832577e326b8b613ea3159
User1012007 <rojasbadilloe@gmail.com>
2025-10-05 10:10:57 -0600

    individual: add control node to adjust the size and place of the text

--- backend/3d/individual/control.gd
+++ backend/3d/individual/control.gd
@@ -1,13 +1,19 @@
 extends Control
 
-@export var metadata_key: String = "name"  # which metadata to read from target node
-@export var target_node_path = "res://backend/3d/individual/"   # path to the planet or info source
+@export var metadata_key: String = "name" 
+@export var target_node_path = "res://backend/3d/individual/" 
 @onready var label: Label = $Label
-
 @export var file_folder: String = "res://backend/3d/individual/info/"
 
+func _ready() -> void:
+	label.visible = false
+
+	
 func _on_button_pressed() -> void:
-	var target_name = get_parent().get_meta(metadata_key)
+	label.visible = !label.visible
+	print(label.visible)
+	var target_name = get_parent().get_parent().get_meta(metadata_key)
+	print("target_name: " + target_name)
 	if target_name == null:
 		push_error("No metadata '%s' found on parent" % metadata_key)
 		return
@@ -20,5 +26,3 @@ func _on_button_pressed() -> void:
 	var file = FileAccess.open(file_path, FileAccess.READ)
 	label.text = file.get_as_text()
 	file.close()
-
-	label.visible = !label.visible

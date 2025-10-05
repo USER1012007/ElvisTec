8068ec41c9c4f5a0109dde82adc72c36e98b7a4b
mike7d7 <mike7d7@proton.me>
2025-10-05 10:01:55 -0600

    solar_sys.tscn + scripts: add toggle for the animation

--- backend/3d/mars_9_ktx.gd
+++ backend/3d/mars_9_ktx.gd
@@ -4,18 +4,18 @@ var angular_speed = 2
 var angle: float = 0.0
 var orbit_radius: float = position.x
 var orbit_speed: float = 0.0
+@export var animation: bool = true
 
 func _ready():
 	orbit_speed = self.get_meta("orbit_speed")
 
 	
 func _process(delta):
-	rotation.y += angular_speed * delta * 1/speed
-	angle += orbit_speed / 100 * delta
+	if animation:
+		rotation.y += angular_speed * delta * 1/speed
+		angle += orbit_speed / 100 * delta
 
-	# compute new x, z
-	var new_x = orbit_radius * cos(angle)
-	var new_z = orbit_radius * sin(angle)
+		var new_x: float = orbit_radius * cos(angle)
+		var new_z: float = orbit_radius * sin(angle)
 
-	# set the global position (y is fixed)
-	position = Vector3(new_x, 0, new_z)
+		position = Vector3(new_x, 0, new_z)
--- backend/3d/sun_real.gd
+++ backend/3d/sun_real.gd
@@ -1,6 +1,8 @@
 extends CSGSphere3D
 var speed = 4
 var angular_speed = PI
+@export var animation: bool = true
 
 func _process(delta):
-	rotation.y += angular_speed * delta * 1/speed
+	if animation:
+		rotation.y += angular_speed * delta * 1/speed

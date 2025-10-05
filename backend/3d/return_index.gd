5265c5037ba5ff6cd0a6e8893128281e918c836c
mike7d7 <mike7d7@proton.me>
2025-10-05 10:44:03 -0600

    return_index.gd

--- backend/3d/return_index.gd
+++ backend/3d/return_index.gd
@@ -1,8 +1,9 @@
 extends Button
+var index = preload("res://frontend/index.tscn")
 
 func _ready():
 	# Conectamos la señal 'pressed' para que detecte el clic
 	self.pressed.connect(_on_pressed)
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://frontend/index.tscn")
+	get_tree().change_scene_to_packed(index)

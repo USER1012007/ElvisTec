9eb9b72d4d425f6501f1a7a20e132a896b5c6dee
mike7d7 <mike7d7@proton.me>
2025-10-04 21:34:48 -0600

    solar_sys.tscn: swap camera3d for a characterBody3d
    allows movement with keys

---
+++ backend/3d/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+const SPEED: int = 50
+@export var mouse_sensitivity: float = 0.2
+@onready var cam: Camera3D = $Node3D/Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(event.relative.x * mouse_sensitivity))
+
+func _physics_process(_delta: float) -> void:
+	var input_dir: Vector2 = Input.get_vector("camera_up", "camera_down", "camera_right", "camera_left")
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if Input.is_action_just_released("planetas_acercar_zoom"):
+		if cam.fov > 10:
+			cam.fov -= 1
+		else:
+			cam.fov -= 0.5
+	if Input.is_action_just_released("planetas_alejar_zoom"):
+		if cam.fov > 10:
+			cam.fov += 1
+		else:
+			cam.fov += 0.5
+	move_and_slide()
